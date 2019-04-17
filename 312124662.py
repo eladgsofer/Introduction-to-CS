ad37a937f90613b9a454181ef8079f3f43c09381
Elad Sofer <elad.sofer@primarydata.com>
2019-04-17 21:26:06 +0300

    boolean matrix

--- 312124662.py
+++ 312124662.py
@@ -11,47 +11,40 @@ class GameOfLife(game_of_life_interface.GameOfLife):
     DEAD_BOOL = 0
     ALIVE_BOOL = 1
     ALIVE_VAL = 255
-    DEAD_OR_ALIVE = [DEAD_BOOL, ALIVE_BOOL]
     PROB_MAPPER = {1: [0.5, 0.5], 2: [0.2, 0.8], 3: [0.8, 0.2]}
+    POS_FUNC_MAPPER = {1: 'init_random_board',   2: 'init_random_board',
+                       3: 'init_random_board', 4: 'init_gosper_glide',
+                       5: 'init_pulsar', 6: 'init_grin'}
 
     def __init__(self, board_size, starting_position=1, rules='B3/S23'):
-        #
         self.board_size = board_size
         self.nxn = tuple([self.board_size] * 2)
-        self.edges = [0, self.board_size-1]
-        # self.offset = tuple([(r,c) for c in r for r in range(3)])
+        self.frame_nxn = tuple([self.board_size + 2] * 2)
+        self.offset = set([(r_offset, c_offset)
+                           for c_offset in range(3)
+                           for r_offset in range(3)]) - {(1,1)}
 
         self.starting_position = starting_position
-        self.position_mapper = {'1': self.__init_random_board,
-                                '2': self.__init_random_board,
-                                '3': self.__init_random_board,
-                                '4': self.__init_gosper_glide,
-                                '5': self.__init_pulsar,
-                                '6': self.__init_grin,
-                                '7': self.__init_test}
+
 
         self.rules = rules
         born, survive = self.rules.split('/')
         self.born = np.array([int(b) for b in born[1:]])
         self.survive = np.array([int(s) for s in survive[1:]])
 
-        self.board  = np.zeros(shape=self.nxn, dtype=int)
-        # board init
-        self.position_mapper[str(self.starting_position)]()
+        self.board  = np.zeros(self.nxn, dtype=int)
+
+        # board initialize
+        getattr(self, self.POS_FUNC_MAPPER[self.starting_position])()
 
     def neighbors_calc(self):
-        neighbors_sum = np.zeros(
-            shape=(self.board_size + 2, self.board_size + 2), dtype=int)
+        neighbors_sum = np.zeros(self.frame_nxn, dtype=int)
 
-        for r_offset in range(3):
-            for c_offset in range(3):
-                if r_offset == 1 and c_offset == 1:
-                    continue
-                tmp_board = np.zeros((self.board_size + 2, self.board_size + 2),
-                                     dtype=int)
-                tmp_board[r_offset:r_offset + self.board_size,
-                c_offset:c_offset + self.board_size] = self.board
-                neighbors_sum += tmp_board
+        for r_offset, c_offset in self.offset:
+            shifted_board = np.zeros(self.frame_nxn, dtype=int)
+            shifted_board[r_offset:r_offset + self.board_size,
+            c_offset:c_offset + self.board_size] = self.board
+            neighbors_sum += shifted_board
 
         return neighbors_sum[1:-1, 1:-1]
 
@@ -61,16 +54,14 @@ class GameOfLife(game_of_life_interface.GameOfLife):
         Output None.
         '''
         neighbors_sum = self.neighbors_calc()
-        dead_neigh_sum = (self.board == self.DEAD_BOOL) * neighbors_sum
-        alive_neigh_sum = (self.board == self.ALIVE_BOOL) * neighbors_sum
 
-        alive_tb = np.isin(alive_neigh_sum, self.survive)
-        dead_tb = np.isin(dead_neigh_sum, self.born)
+        alive_tb = np.isin(neighbors_sum, self.survive)
+        dead_tb = np.isin(neighbors_sum, self.born)
 
-        alive_tb = alive_tb*self.board
+        dead_neigh_sum = (self.board == self.DEAD_BOOL) * dead_tb
+        alive_neigh_sum = (self.board == self.ALIVE_BOOL) * alive_tb
 
-        self.board = (alive_tb+dead_tb)
-
+        self.board = (dead_neigh_sum+alive_neigh_sum)
 
 
     def save_board_to_file(self, file_name):
@@ -86,9 +77,7 @@ class GameOfLife(game_of_life_interface.GameOfLife):
         Input None.
         Output a figure should be opened and display the board.
         '''
-        # plt.interactive(True)
         plt.show(plt.matshow(self.board, fignum=self.FIGNUM))
-        # plt.show()
 
     def return_board(self):
         ''' This method returns a list of the board position. The board is a two-dimensional list that every
@@ -98,7 +87,7 @@ class GameOfLife(game_of_life_interface.GameOfLife):
         '''
         return (self.board*self.ALIVE_VAL).tolist()
 
-    def __init_pulsar(self):
+    def init_pulsar(self):
 
         center = self.board_size//2
 
@@ -116,12 +105,12 @@ class GameOfLife(game_of_life_interface.GameOfLife):
         for y, x in hor_start_points:
             self.board[y,x:x+3] = self.ALIVE_BOOL
 
-    def __init_gosper_glide(self):
+    def init_gosper_glide(self):
         GOSPER_GLIDER_INDEXS = [(14, 10), (14, 11), (15, 10), (15, 11), (14, 20),
                                 (15, 20), (16, 20), (13, 21), (17, 21), (12, 22),
                                 (18, 22), (12, 23), (18, 23), (15, 24), (13, 25),
                                 (17, 25), (15, 27), (11, 32), (15, 32)]
-        for r,c in GOSPER_GLIDER_INDEXS:
+        for r, c in GOSPER_GLIDER_INDEXS:
             self.board[r,c] = self.ALIVE_BOOL
 
         self.board[10:12, 34] = self.ALIVE_BOOL
@@ -132,34 +121,24 @@ class GameOfLife(game_of_life_interface.GameOfLife):
         self.board[12:15, 30] = self.ALIVE_BOOL
         self.board[12:15, 31] = self.ALIVE_BOOL
 
-    def __init_grin(self):
+    def init_grin(self):
         GRIN_INDEXES = [(5, 5), (5, 8), (6, 6), (6, 7)]
         for r,c in GRIN_INDEXES:
             self.board[r,c] = self.ALIVE_BOOL
 
-    def __init_test(self):
-        np.zeros(self.nxn)
-        GRIN_INDEXES = [(1, 1), (1, 2), (1, 3)]
-        for r,c in GRIN_INDEXES:
-            self.board[r,c] = self.ALIVE_BOOL
-
-
-    def __init_random_board(self):
-        self.board = np.random.choice(self.DEAD_OR_ALIVE, size=self.nxn,
+    def init_random_board(self):
+        self.board = np.random.choice([self.DEAD_BOOL, self.ALIVE_BOOL], size=self.nxn,
                                       p=self.PROB_MAPPER[self.starting_position])
-
-
 
 
 
 if __name__ == '__main__':
     import time
     t1= time.time()
-    a = GameOfLife(board_size=200, starting_position=1,
-                   rules='B2/S0')
-    for i in range(5):
+    a = GameOfLife(board_size=999, starting_position=4,
+                   rules='B3/S23')
+    for i in range(600):
         a.update()
-        a.display_board()
     print (time.time()-t1)
     a.save_board_to_file('1000.png')
 
